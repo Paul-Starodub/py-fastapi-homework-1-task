019efb7e52d348a4ef258ffcec07edc81e7df744
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-01-30 23:54:16 +0200

    add routers for movies CRUD operations

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -5,18 +5,18 @@ from pydantic import BaseModel, Field, ConfigDict
 
 class MovieModelBase(BaseModel):
     id: int
-    name: str = Field(min_length=1, max_length=255)
+    name: str = Field(max_length=255)
     date: date
     score: float
-    genre: str = Field(min_length=1, max_length=255)
-    overview: str = Field(min_length=1)
-    crew: str = Field(min_length=1)
-    orig_title: str = Field(min_length=1, max_length=255)
-    status: str = Field(min_length=1, max_length=50)
-    orig_lang: str = Field(min_length=1, max_length=50)
+    genre: str = Field(max_length=255)
+    overview: str
+    crew: str
+    orig_title: str = Field(max_length=255)
+    status: str = Field(max_length=50)
+    orig_lang: str = Field(max_length=50)
     budget: Decimal = Field(max_digits=12, decimal_places=2)
     revenue: float
-    country: str = Field(min_length=1, max_length=3)
+    country: str = Field(max_length=3)
 
     model_config = ConfigDict(from_attributes=True, json_encoders={Decimal: float})
 
